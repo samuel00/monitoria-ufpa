618131f6531864a34c33a3229b7e0777ffb2bbb5
samuel00 <ifpa.santana@gmail.com>
2018-04-30 18:08:21 -0300

    Adicao da tarefa do dia 30-01-2018

---
+++ src/main/java/br/com/sls/monitoria/principal/TestaCriacaoDeObjetos.java
@@ -0,0 +1,33 @@
+package br.com.sls.monitoria.principal;
+
+public class TestaCriacaoDeObjetos {
+	
+	public static void main(String [] args){
+		//01 Criar no pacote modelo uma classe chamada CartaoRefeicao
+		//02 Criar no pacote modelo uma classe chamada PessoaFisica que vai utilizar o cartao
+		//03 Criar no pacote modelo uma classe chamada Mercadoria
+		
+		//Obs 01 A classe CartaoRefeicao Deve ter como atributos privados numeracao, nome, senha, saldo
+		//Obs 02 A classe CartaoRefeicao Deve ter como metodos publicos os metodos para acessar os seus atributos
+		
+		//Obs 03 A classe PessoaFisica Deve ter como atributos privados nome, cpf, cartaoRefeicao
+		//Obs 04 A classe PessoaFisica Deve ter como metodos publicos os metodos para acessar os seus atributos
+		
+		//Obs 05 A classe Mercadoria deve ter como atributos privados preco e descricao
+		//Obs 06 A classe Mercadoria Deve ter como metodos publicos os metodos para acessar os seus atributos
+		
+		/*
+		 * Tarefa 01 - Implementar um programa que possa realizar a compra de TRÊS mercadorias atraves do cartao 
+		 * refeicao	de uma pessoa física. O processo de compra deve solicitar o numero do cartao, a senha 
+		 * do cartao, o valor da mercadoria e verificar se a pessoa possui saldo suficiente para realizar 
+		 * a compra.
+		 * 
+		 * No final do processo deve aparecer uma mensagem informando o nome e cpf da pessoa assim como o 
+		 * saldo atual do cartao. */
+		
+		/** Comece aqui sua classe Principal*/
+		
+		
+	}
+
+}
